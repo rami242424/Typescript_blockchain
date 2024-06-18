a4245816f56f49f78b146dc177db8c0b13e199ee
Rami <138556019+rami242424@users.noreply.github.com>
2024-06-19 01:20:45 +0900

    #5.4 JSDoc

--- src/myPackage.js
+++ src/myPackage.js
@@ -1,7 +1,23 @@
-export function(config) { 
+// @ts-check 
+// 타입스크립트 파일한테 자바스크립트 파일을 확인하라고 알리는 것
+
+/**
+ * Initializes the project
+ * @param {object} config 
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean}
+ */
+export function init(config) { 
     return true;
 }
 
+
+/**
+ * Exit the program
+ * @param {number} code 
+ * @returns {number}
+ */
 export function exit(code) {
     return code + 1;
 }
